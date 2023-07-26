624454ca7f7ef71659a5805309530fa00abe17ac
Arjun024 <mallikarjuna.gunisetti@gmail.com>
2023-07-26 13:00:32 +0530

    feat: registration page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { TextField, FormControl, Button } from '@mui/material';
+
 
 const RegisterPage = () => {
   return (
-    <div>
-      Register Page
+    <div className="register-page">
+      <div className="register-container">
+        <div className="register-form">
+          <h1>Sign Up</h1>
+          <FormControl fullWidth>
+            <TextField id="filled-basic" label="Full Name" variant="filled" />
+            <TextField id="filled-basic" label="Email" variant="filled" />
+            <TextField id="filled-basic" label="Phone" type='number' variant="filled" />
+            <TextField id="filled-basic" label="Username" variant="filled" />
+            <TextField id="filled-basic" label="Password" type='password' variant="filled" />
+            <Button sx={{marginTop: '20px'}} variant="contained">Submit</Button>
+          </FormControl>
+        </div>
+      </div>
     </div>
   )
 }
