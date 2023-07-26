0942f6b438c3350d24149af90321da06caf18eab
Arjun024 <mallikarjuna.gunisetti@gmail.com>
2023-07-26 11:06:17 +0530

    feat: footer

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div className="footer-container">
+        <p className="footer-text">
+          Made with ❤️ by <a href="https://github.com/Arjun-024" target='_blank' ><u><i>Arjun</i></u></a>
+        </p>
+      </div>
+    </footer>
   )
 }
 
